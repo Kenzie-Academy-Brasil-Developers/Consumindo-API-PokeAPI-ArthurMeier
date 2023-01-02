e80e94b4c994fb80f522ce243e09786da8e238a6
Arthur Meier <Arthurcmeier@outlook.com>
2023-01-01 22:09:40 -0300

    falta so a pesquisa

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,26 @@
-/* Monte sua lógica aqui */
+const input = document.querySelector('.pesquisa')
+
+async function renderizarPokemons() {
+    const lista = document.querySelector('ul')
+
+    const listaDePokemons = await getPokemons()
+    setTimeout(() => {
+        listaDePokemons.results.forEach(pokemon => {
+            const numeroNaPokedex = pokemon.url.slice(34, -1)
+
+            lista.insertAdjacentHTML('beforeend', `
+            <li>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name}>
+                <p>${pokemon.name}</p>
+            </li>
+        `)
+        })
+    }, 1000);
+}
+
+renderizarPokemons()
+
+input.onkeyup = function(e){
+    t=this.value
+    console.log(t)
+}
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,20 @@
-/* Monte a lógica das requisições aqui */
+async function getPokemons() {
+    const loading = document.querySelector('.loading')
+
+    const listaDePokemons = await fetch('https://pokeapi.co/api/v2/pokemon')
+        .then(resposta => {
+            return resposta.json()
+        })
+        .then( resposta =>{
+            setTimeout(()=>{
+                loading.remove()
+            },1000)
+
+            return resposta
+        })
+      .catch (error => console.log(error))
+
+    return listaDePokemons
+}
+
+getPokemons()
