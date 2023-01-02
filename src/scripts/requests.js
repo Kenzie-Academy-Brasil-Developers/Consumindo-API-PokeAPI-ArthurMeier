69242995dbbe87de3cfbc1225a3d4584b5cf2cae
Arthur Meier <Arthurcmeier@outlook.com>
2023-01-01 22:14:46 -0300

    pequena correcao no limite de pokemons

--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,7 +1,7 @@
 async function getPokemons() {
     const loading = document.querySelector('.loading')
 
-    const listaDePokemons = await fetch('https://pokeapi.co/api/v2/pokemon')
+    const listaDePokemons = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
         .then(resposta => {
             return resposta.json()
         })
